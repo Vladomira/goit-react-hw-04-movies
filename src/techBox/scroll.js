58fdf40913a7cf2df03752817117168b97880b65
Vladomira <juliavladomira@gmail.com>
2021-11-03 17:00:12 +0300

    corrects scroll

--- src/techBox/scroll.js
+++ src/techBox/scroll.js
@@ -1,6 +1,7 @@
 const scroll = () => {
   window.scrollTo({
-    top: document.documentElement.scrollHeight,
+    // top: document.documentElement.scrollHeight,
+    top: 1000,
     behavior: 'smooth',
   });
 };
